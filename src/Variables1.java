29d5f3004d862430c9d58ab39c1def9d90d1c479
DiegoHernandez <diego0930@gmail.com>
2024-08-19 08:59:55 -0500

    Ejercicio variables

---
+++ src/Variables1.java
@@ -0,0 +1,29 @@
+/*PRACTICE EXERCISE
+Variables
+Se te da un código que genera las propiedades de un vehículo, pero algo está mal.
+
+Tarea
+Arregla el código para imprimir las propiedades.
+
+Salida
+Name: Toyota
+Engine: 4.7
+Year: 2019
+
+	}
+*/
+
+public class Variables1 {
+	//arregla los tipos de variables 
+	public static void main(String[] args) {
+			String  name = "Toyota";
+		     double engine = 4.7;
+		     int year = 2019;
+		    
+		     System.out.println("Name: " + name);
+		     System.out.println("Engine: " + engine);
+		     System.out.println("Year: " + year);
+
+	}
+
+}
